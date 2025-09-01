37f2c5c3d0e5e397822fe4f1d4a9b17eb80d9b6a
aestene <arnts@equinor.com>
2025-09-01 15:56:40 +0200

    Add endpoint for retrieving CO2 measurements

--- sara_timeseries/api.py
+++ sara_timeseries/api.py
@@ -1,8 +1,16 @@
+from typing import List, Dict
+
 from fastapi import APIRouter, FastAPI, HTTPException
 from loguru import logger
 from omnia_timeseries.api import MessageModel
+from omnia_timeseries.models import TimeseriesModel
 
-from sara_timeseries.models import RequestModel, ResponseModel
+from sara_timeseries.models import (
+    RequestModel,
+    ResponseModel,
+    DatapointsRequestModel,
+    DatapointsResponseModel,
+)
 from sara_timeseries.omnia_service import OmniaService
 
 
@@ -59,6 +67,44 @@ class OmniaAPI:
                 statusCode=response["statusCode"],
                 message=response["message"],
             )
+
+        @self.router.post("/get-co2-measurements")
+        def get_co2_measurements(
+            request: DatapointsRequestModel,
+        ) -> DatapointsResponseModel:
+            co2_measurements_description: str = "CO2Measurement"
+            try:
+                timeseries: List[TimeseriesModel] = (
+                    self.omnia_service.read_timeseries_by_description_and_facility(
+                        description=co2_measurements_description,
+                        facility=request.facility,
+                    )
+                )
+            except Exception:
+                logger.error(
+                    f"Failed to retrieve timeseries for description {co2_measurements_description} "
+                    f"and facility {request.facility}"
+                )
+                raise HTTPException(
+                    status_code=500, detail="Failed to retrieve all timeseries"
+                )
+
+            try:
+                data: List[Dict] = (
+                    self.omnia_service.read_data_from_multiple_timeseries(
+                        timeseries=timeseries,
+                        start_time=request.start_time,
+                        end_time=request.end_time,
+                    )
+                )
+                return DatapointsResponseModel(data=data)
+
+            except Exception:
+                logger.error("Failed to retrieve data from CO2 measurement timeseries")
+                raise HTTPException(
+                    status_code=500,
+                    detail="Failed to retrieve data from CO2 measurement timeseries",
+                )
 
     def include_in_app(self, app: FastAPI) -> None:
         app.include_router(self.router)
