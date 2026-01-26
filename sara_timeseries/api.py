2d939ae667ef844326128a53571fec8751a6b6c9
Ola Alstad <OALST@equinor.com>
2026-01-26 22:23:45 +0100

    Fix opentelemetry already instrumented warning

--- sara_timeseries/api.py
+++ sara_timeseries/api.py
@@ -1,3 +1,5 @@
+from typing import Any, Callable, Optional
+
 from fastapi import FastAPI
 
 from sara_timeseries.modules.sara_timeseries_api.timeseries_controller import (
@@ -17,8 +19,10 @@ class API:
         self.timeseries_controller: TimeseriesController = timeseries_controller
         self.insights_controller: InsightsController = insights_controller
 
-    def create_app(self) -> FastAPI:
-        app = FastAPI()
+    def create_app(
+        self, lifespan: Optional[Callable[[FastAPI], Any]] = None
+    ) -> FastAPI:
+        app = FastAPI(lifespan=lifespan)
         app.include_router(router=self.timeseries_controller.create_timeseries_router())
         app.include_router(router=self.insights_controller.create_insights_controller())
         return app
